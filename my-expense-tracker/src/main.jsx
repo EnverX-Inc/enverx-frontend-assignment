011e5b4273545a0090463069e744998973817e13
imcodekhan <imkhan.mdamir@outlook.com>
2023-06-30 12:04:43 +0530

    wrapped main with provider

--- my-expense-tracker/src/main.jsx
+++ my-expense-tracker/src/main.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import Dashboard from "./Containers/Dashboard";
-
+import { Provider } from "react-redux";
+import store from "./Redux/store";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Dashboard />
+    <Provider store={store}>
+      <Dashboard />
+    </Provider>
   </React.StrictMode>
 );
